e66e3d57d58b25e2fcfe4fe3918f40b06c44c725
Chang-Jin-Lee <ckdwls525@gmail.com>
2025-10-05 20:42:18 +0900

    [refactor] update VertexTriangle in 01_RenderingQuadangle

--- Common/Vertex.h
+++ Common/Vertex.h
@@ -1,5 +1,18 @@
 #pragma once
 
+struct VertexTriangle
+{
+	// 정점의 위치, 색상 정보
+	Vector3 position;
+	Vector3 color;
+
+	VertexTriangle(float x, float y, float z, float r, float g, float b) : position(x, y, z), color(r, g, b) {}
+	VertexTriangle(Vector3 pos) : position(pos) {}
+	VertexTriangle(Vector3 pos, Vector4 col) : position(pos), color(col.x, col.y, col.z) {}
+};
+
+
+
 struct VertexTBN
 {
 	DirectX::XMFLOAT3 pos;
